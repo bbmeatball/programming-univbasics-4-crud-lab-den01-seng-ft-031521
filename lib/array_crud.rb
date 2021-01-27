a51a2c088cbb6d577cf43fb26f2f447ea645c82d
JoAnn Siciliano <joannsiciliano@gmail.com>
2021-01-27 21:48:19 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -44,5 +44,6 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-
+hello_array=["hi","helo","howru","imok","okcool"]
+hello_array[4]="totally"
 end
