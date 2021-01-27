aef4cc576a9b8129942b7397a447fd5bdaa60456
JoAnn Siciliano <joannsiciliano@gmail.com>
2021-01-27 21:45:16 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -39,7 +39,8 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  
+  last_array=["asdf","asdfadf","sdafaf","asdf","totally"]
+  last_array[4]
 end
 
 def update_element_from_index(array, index_number, element)
